96f435271041d457a1e077ae270b18453a242345
Marcel-12345 <anselmus.marcel@gmail.com>
2026-04-27 22:55:09 +0700

    Pertemuan 5 Jobsheet 2

--- app/Filament/Resources/Users/Schemas/UserForm.php
+++ app/Filament/Resources/Users/Schemas/UserForm.php
@@ -3,14 +3,27 @@
 namespace App\Filament\Resources\Users\Schemas;
 
 use Filament\Schemas\Schema;
+use Filament\Forms\Components\TextInput;
 
 class UserForm
 {
+
     public static function configure(Schema $schema): Schema
     {
         return $schema
             ->components([
-                //
+                TextInput::make('name')
+                    ->required()
+                    ->maxLength(255),
+                TextInput::make('email')
+                    ->email()
+                    ->unique()
+                    ->required()
+                    ->maxLength(255),
+                TextInput::make('password')
+                    ->password()
+                    ->required()
+                    ->minLength(6)
             ]);
     }
 }
